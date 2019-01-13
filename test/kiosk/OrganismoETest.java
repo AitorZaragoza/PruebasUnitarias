a063f9ccceffa9173f507fd4586fb30beb07141b
AitorZaragoza <AitorZaragoza@users.noreply.github.com>
2019-01-13 23:02:00 +0100

    P3

--- test/kiosk/OrganismoETest.java
+++ test/kiosk/OrganismoETest.java
@@ -1,22 +1,32 @@
 package kiosk;
 
 
+import data.DigitalSignature;
 import data.Nif;
+import data.Party;
 import org.junit.jupiter.api.Test;
-import kiosk.OrganismoE;
-import services.ElectoralOrganism;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrganismoETest {
+    OrganismoE organism = new OrganismoE();
 
     @Test
     public void canVoteTest(){
         Nif niftrue = new Nif("73564125B");
         Nif niffalse = new Nif("78452314M");
-        OrganismoE organism = new OrganismoE();
 
         assertEquals(true, organism.canVote(niftrue));
         assertEquals(false, organism.canVote(niffalse));
+    }
+
+    @Test
+    public void askForDigitalSignatureTest(){
+        Party PP = new Party("PP");
+        DigitalSignature ds = new DigitalSignature("PP".getBytes());
+
+        assertEquals(ds, organism.askForDigitalSignature(PP));
+
     }
 }
